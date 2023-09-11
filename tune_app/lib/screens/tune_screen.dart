d9a58bd9fa84f52dd6c60acbf5c8e2479fbd2036
MohamedElmelegy74 <75188929+MohamedElmelegy74@users.noreply.github.com>
2023-09-11 13:42:15 +0300

    frist commit

---
+++ tune_app/lib/screens/tune_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TuneScreen extends StatelessWidget {
+  const TuneScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Tune'),
+        backgroundColor: Colors.blue,
+      ),
+    );
+  }
+}
